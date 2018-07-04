0f756da1707ae174afd6463fd3ad41354a50e78a
MikeW <uce_mike@yahoo.com>
2018-07-04 16:42:49 -0500

    2.5 removed old tmp node code, replaced with associative array/table
    2.5 removed old tmp node code, replaced with associative array/table

--- scripts/manager_author_adnd.lua
+++ scripts/manager_author_adnd.lua
@@ -46,55 +46,58 @@ function performRefIndexBuild()
 
   -- pickup all stories
   -- there is probably a better way to do this, table of rRecords?
+  aStoryCategories = {};
+  --
   local dStoryRaw = DB.getChildren("encounter");
-  local dRoot = DB.createChild("_authorRefmanual_tmp");
-  local dStories = DB.createChild(dRoot,"stories");
-  local dStoryCategories = DB.createChild(dStories,"category");
+  -- local dRoot = DB.createChild("_authorRefmanual_tmp");
+  -- local dStories = DB.createChild(dRoot,"stories");
+  -- local dStoryCategories = DB.createChild(dStories,"category");
 
   for _,node in pairs(dStoryRaw) do
     local sCategory = UtilityManager.getNodeCategory(node);
     -- only apply if the record is in a category
     if (sCategory ~= "") then
-      -- strip out all periods because we use category name as a child/node name --DO SOMETHING ELSE
-      sCategory = sCategory:gsub("%.",""); 
-      local dCategory = DB.getChild(dStoryCategories,sCategory);
-      if (dCategory == nil) then
-        dCategory = DB.createChild(dStoryCategories,sCategory);
-        DB.setValue(dCategory,"name","string",sCategory);
+      -- -- strip out all periods because we use category name as a child/node name --DO SOMETHING ELSE
+      -- sCategory = sCategory:gsub("%.",""); 
+      --
+      if aStoryCategories[sCategory] == nil then
+        aStoryCategories[sCategory] = {};
       end
-      local nodeEntry = dCategory.createChild();
-      DB.copyNode(node,nodeEntry);
+      aStoryCategories[sCategory][node.getPath()] = DB.getValue(node,"name","");
       -- local sNodeID = node.getPath():match("(id%-%d+)$");
       -- if (sNodeID) then
         -- DB.setValue(nodeEntry,"_sourceNode","string",sNodeID);
       -- end
     end
   end
-
   -- reference section
   local nodeRefIndex = DB.createNode(sTmpRefIndexName);
   local nodeChapters = DB.createChild(nodeRefIndex,"chapters");
   -- flip through all categories, create sub per category and and entries within category
-  for _,nodeCategory in pairs(sortByName(dStoryCategories.getChildren())) do
+  for _,sCatagoryName in pairs(sortCatagoriesByName(aStoryCategories)) do
+--Debug.console("manager_author_adnd.lua","performRefIndexBuild","sCatagoryName",sCatagoryName);    
     -- create chapter for this category
-    local sChapterName = DB.getValue(nodeCategory,"name","EMPTY-CATEGORY-NAME");
+    local sChapterName = sCatagoryName;
     local nodeChapter = DB.createChild(nodeChapters);
     local sCleanChapterName = sChapterName;
-    --if (aProperties.bStripOrderingChapter) then
-      sCleanChapterName = stripLeadingNumbers(sChapterName)
-    --end
+    sCleanChapterName = stripLeadingNumbers(sChapterName)
+--Debug.console("manager_author_adnd.lua","performRefIndexBuild","sCleanChapterName",sCleanChapterName);    
     DB.setValue(nodeChapter,"name","string",sCleanChapterName);
     -- create subchapter for this category (have to have sub in every chapter)
     local nodeSubChapters = DB.createChild(nodeChapter,"subchapters");
     -- store this outside of the nodeStory function so we only have aExportSources
     -- sub-chapter per chapter unless <sub> string found in story name
     local nodeSubChapter = nil;
-    for _,nodeStory in pairs(sortByName(nodeCategory.getChildren())) do
+    for _,sSourceNode in pairs(sortStoriesByName(aStoryCategories[sCatagoryName])) do
+--Debug.console("manager_author_adnd.lua","performRefIndexBuild","sSourceNode",sSourceNode);    
+      local nodeStory = DB.findNode(sSourceNode);
+      if (nodeStory) then
         local sNodeName = DB.getValue(nodeStory,"name","");
+--Debug.console("manager_author_adnd.lua","performRefIndexBuild","sNodeName1",sNodeName);    
         --local sNodeID = DB.getValue(nodeStory,"_sourceNode","");
 --Debug.console("author.lua","performExport","sNodeID",sNodeID);         
         if (sNodeName ~= "") then
---Debug.console("author.lua","performExport","sNodeName",sNodeName); 
+--Debug.console("manager_author_adnd.lua","performRefIndexBuild","sNodeName2",sNodeName); 
           -- store current subchapter node in "local" var
           -- so we can replace with new one if we get <sub>
           local nodeSubChapterSub =  nodeSubChapter;
@@ -143,21 +146,12 @@ function performRefIndexBuild()
             DB.setValue(nodeBlock,"text","formattedtext",DB.getValue(nodeStory,"text","EMPTY-STORY-TEXT"));
           --end
           DB.setValue(nodeRefPage,"listlink","windowreference",sLinkClass,sLinkRecord);
-      end
-    end
-  end
+        end -- sNodeName
+      end -- nodeStory
+    end -- for
+  end -- for
 
   ChatManager.SystemMessage("AUTHOR: created " .. sTmpRefIndexName .. " entries for export.");
-end
-
--- pass list of nodes with a "name" record and sort by name
-function sortByName(nodes)
-  local aSorted = {};
-  for _,node in pairs(nodes) do
-    table.insert(aSorted, node);
-  end        
-  table.sort(aSorted, function (a, b) return DB.getValue(a,"name","") < DB.getValue(b,"name","") end);
-  return aSorted;
 end
 
 -- remove leading \d+ and punctuation on text and return it
@@ -167,4 +161,21 @@ function stripLeadingNumbers(sText)
       sText = sTextTrimmed;
     end
     return sText;
+end
+
+function sortCatagoriesByName(aCatagory)
+  local aSorted = {};
+  for sCatagoryName,aStoryEntries in pairs(aCatagory) do
+    table.insert(aSorted, sCatagoryName);
+  end        
+  table.sort(aSorted);
+  return aSorted;
+end
+function sortStoriesByName(aStories)
+  local aSorted = {};
+  for sStorySource,sStoryName in pairs(aStories) do
+    table.insert(aSorted, sStorySource);
+  end
+  table.sort(aSorted, function(a, b) return aStories[a] < aStories[b] end);
+  return aSorted;
 end
